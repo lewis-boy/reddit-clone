51a448d663145905346e040aaee7921f30c675d0
Luis Escobar <luisaeniebla@gmail.com>
2023-07-05 19:42:23 -0700

    We can now join and leave a subreddit. We created the button to write a post. Overall, created basic layout of subreddits

---
+++ src/app/api/subreddit/unsubscribe/route.ts
@@ -0,0 +1,65 @@
+import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db"
+import { SubredditSubscriptionSchema } from "@/lib/validators/subreddit"
+import { z } from "zod"
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession()
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 })
+    }
+
+    const body = await req.json()
+    const { subredditId } = SubredditSubscriptionSchema.parse(body)
+
+    const subscriptionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    })
+
+    if (!subscriptionExists) {
+      return new Response("You are not subscribed to this subreddit", {
+        status: 400,
+      })
+    }
+
+    //check is user is the creator of the subreddit
+    const subreddit = await db.subreddit.findFirst({
+      where: {
+        id: subredditId,
+        creatorId: session.user.id,
+      },
+    })
+
+    if (subreddit) {
+      return new Response("You can't unsubscribe from your own subreddit", {
+        status: 400,
+      })
+    }
+
+    await db.subscription.delete({
+      where: {
+        userId_subredditId: {
+          subredditId,
+          userId: session.user.id,
+        },
+      },
+    })
+
+    return new Response(subredditId)
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      //parsing failed and wrong data was sent to us
+      //send 422. unprocessable entity // or a bad status code
+      return new Response("Invalid request data passed ", { status: 422 })
+    }
+
+    return new Response("Could not unsubscribe, please try again later", {
+      status: 500,
+    })
+  }
+}
---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,52 @@
+import MiniCreatePost from '@/components/MiniCreatePost'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
+import { FC } from 'react'
+
+interface PageProps {
+    params: {
+        slug: string
+    }
+}
+
+const page = async ({ params }: PageProps) => {
+    const { slug } = params
+    //we want to change up what is displayed based on whether they are authenticated or not
+    const session = await getAuthSession()
+
+    const subreddit = await db.subreddit.findFirst({
+        where: {
+            name: slug,
+        },
+        include: {
+            posts: {
+                include: {
+                    author: true,
+                    votes: true,
+                    comments: true,
+                    subreddit: true
+                },
+                take: INFINITE_SCROLLING_PAGINATION_RESULTS
+            }
+        }
+    })
+
+    if (!subreddit) {
+        //throw a 404 when no subreddit with that name exists
+        return notFound()
+    }
+
+    return (
+        <>
+            <h1 className='font-bold text-3xl md:text-4xl h-14'>
+                r/{subreddit.name}
+            </h1>
+            <MiniCreatePost session={session} />
+            {/* TODO: Show posts in user feed */}
+        </>
+    )
+}
+
+export default page
